28f44987538aa28c25238ac307d606c55063fb70
DStukalo <94228608+DStukalo@users.noreply.github.com>
2023-03-20 14:25:19 +0200

    feat: add types

---
+++ rick-and-morty/src/types/types.ts
@@ -0,0 +1,26 @@
+export type DetailsData = {
+	id: number,
+	name: string,
+	status:	string,
+	species:	string,
+	type:	string,
+	gender:	string,
+	origin: {
+		name: string
+	},
+	location:	object,
+	image:	string,
+	episode:	string[],
+	url:	string,
+	created:	string,
+};
+
+export type CharacterData = {
+	info: {
+		count: number;
+		next: string;
+		pages: number;
+		prev: string | null
+	},
+	results: DetailsData[]
+}
