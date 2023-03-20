5e563b564f0340d91144b8a97f8a8108b611cba3
DStukalo <94228608+DStukalo@users.noreply.github.com>
2023-03-20 14:39:30 +0200

    feat: add App component

---
+++ rick-and-morty/src/App.tsx
@@ -0,0 +1,25 @@
+import {
+	Route, createBrowserRouter, createRoutesFromElements, RouterProvider,
+} from 'react-router-dom';
+import { MainPage, mainLoader } from './pages/MainPage/MainPage';
+import { DetailsPage, detailsLoader } from './pages/DetailsPage/DetailsPage';
+import { NotFoundPage } from './pages/404/NotFoundPage';
+import { Layout } from './components/Layout/Layout';
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path="/" element={<Layout />}>
+			<Route index element={<MainPage />} loader={mainLoader} />
+			<Route path="/details/:id" element={<DetailsPage />} loader={detailsLoader} />
+			<Route path="*" element={<NotFoundPage />} />
+		</Route>,
+	),
+);
+
+function App() {
+	return (
+		<RouterProvider router={router} />
+	);
+}
+
+export default App;
