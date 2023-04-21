1bcfadc7bddb19c7ebaeb84ba2d27977d9e5f299
DStukalo <94228608+DStukalo@users.noreply.github.com>
2023-04-21 16:14:58 +0300

    refactor: update store

--- rick-and-morty/src/store/store.ts
+++ rick-and-morty/src/store/store.ts
@@ -1,34 +1,23 @@
 import { create } from 'zustand';
-import { CharacterData } from '../types/types';
-import { sortByName } from '../function/sortByName';
 
-type CharactersState = {
-	characters: CharacterData | object,
-	loading: boolean,
-	errorMessage: string,
-	curPage: string | null,
-	fetchCharacters: (arg0: string) => Promise<void>,
-	updateCurPage: (arg0: string) => void
+type AuthState = {
+	isAuth: boolean,
+	userName: string | null,
+	updateAuthStatusToTrue: () => void,
+	updateAuthStatusToFalse: () => void,
+	setUserName: (newName: string | null) => void
 }
 
-export const useCharacters = create<CharactersState>((set) => ({
-	characters: {},
-	loading: true,
-	errorMessage: '',
-	curPage: null,
-	fetchCharacters: async (url: string) => {
-		set({ loading: true });
-		set({ errorMessage: '' });
-		try {
-			const res = await fetch(url);
-			set({ characters: sortByName(await res.json()) });
-		} catch (error) {
-			set({ errorMessage: 'We don`t find anything in db' });
-		} finally {
-			set({ loading: false });
-		}
+export const useAuth = create<AuthState>((set) => ({
+	isAuth: false,
+	userName: null,
+	updateAuthStatusToTrue: () => {
+		set({ isAuth: true });
 	},
-	updateCurPage: (link:string) => {
-		set({ curPage: link });
+	updateAuthStatusToFalse: () => {
+		set({ isAuth: false });
+	},
+	setUserName: (newName) => {
+		set({ userName: newName });
 	},
 }));
