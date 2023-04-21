9f9adf71ec17abf13367dd04ec6a8fe290d0d7c4
DStukalo <94228608+DStukalo@users.noreply.github.com>
2023-04-21 16:25:44 +0300

    refactor: update serch component

--- rick-and-morty/src/components/SearchItem/SearchItem.tsx
+++ rick-and-morty/src/components/SearchItem/SearchItem.tsx
@@ -1,31 +1,34 @@
 import { useEffect, useState } from 'react';
-import styles from './SearchItem.module.scss';
+import { useParams, useNavigate } from 'react-router-dom';
 import { useDebounce } from '../../function/useDebounce';
-import { useCharacters } from '../../store/store';
+import styles from './SearchItem.module.scss';
 
 export function SearchItem() {
-	const { fetchCharacters, updateCurPage } = useCharacters((state) => ({
-		fetchCharacters: state.fetchCharacters,
-		updateCurPage: state.updateCurPage,
-	}));
-	const [searchValue, setSearchValue] = useState(sessionStorage.getItem('searchValue') || '');
+	const { search, page } = useParams();
+	const [searchValue, setSearchValue] = useState(search || '');
+	const [isTap, setIsTap] = useState(false);
+	const navigate = useNavigate();
 	const debouncedUpdateValueInput = useDebounce(searchValue as string, 500);
 
+	function setNavigate() {
+		if (page && !isTap) navigate(`/${page}/${searchValue}`);
+		else navigate(`/1/${searchValue}`);
+		setIsTap(false);
+	}
+
 	useEffect(() => {
-		if (!sessionStorage.getItem('searchValue')) updateCurPage('');
-		fetchCharacters(sessionStorage.getItem('searchValue') ? `https://rickandmortyapi.com/api/character?name=${sessionStorage.getItem('searchValue')}`
-			: 'https://rickandmortyapi.com/api/character');
+		setNavigate();
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [debouncedUpdateValueInput]);
 
 	function handleChange(event: { target: HTMLInputElement }) {
-		sessionStorage.setItem('searchValue', event?.target.value);
-		setSearchValue(sessionStorage.getItem('searchValue') || '');
+		setSearchValue(event?.target.value);
+		setIsTap(true);
 	}
 
 	return (
-		<div className={styles.search}>
+		<section className={styles.search}>
 			<input type="search" placeholder="Filter by name..." onChange={handleChange} defaultValue={searchValue as string} />
-		</div>
+		</section>
 	);
 }
